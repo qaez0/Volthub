77304a2005c8ee11dc6e064065d7468d60141f2b
Xyrelll <ella587620@gmail.com>
2025-12-12 12:14:51 +0800

    UPDATE: Google map location

--- src/components/common/LocationMap.tsx
+++ src/components/common/LocationMap.tsx
@@ -8,8 +8,7 @@ export default function LocationMap({
   return (
     <div className={`w-full h-[400px] rounded-lg shadow-md overflow-hidden ${className}`}>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d241.36881101722355!2d121.05188277169493!3d14.547654973393401!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c940ee8ad6cf%3A0x768c8d76fadf4ade!2sHigh%20Street%20South%20Corporate%20Plaza%20Tower%202%20-%20Parking!5e0!3m2!1sen!2sph!4v1764387801938!5m2!1sen!2sph"
-        width="100%"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d277.32804973049855!2d121.05169681676608!3d14.547769512742596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c8edec27e7c1%3A0xb605e47597d14224!2sHigh%20Street%20South%20Corporate%20Plaza%20Tower%2C%202%2026th%20St%2C%20Taguig%2C%20Makati%20City%2C%201631%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1765512708682!5m2!1sen!2sph"
         height="100%"
         style={{ border: 0 }}
         allowFullScreen
